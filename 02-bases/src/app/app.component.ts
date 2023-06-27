61adf43f9e76861510db495c501f0843d818c3bc
Luciano de la Rubia <delarubialuciano@gmail.com>
2023-06-27 19:21:14 -0300

    bases

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -9,25 +9,7 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   public title: string  = 'Hola Mundo';
-  public counter:number = 10;
 
-  public increaseBy(value:number):void{
-
-    // if (value === 1) {
-    //   this.counter += 1;
-    // } else {
-    //   this.counter -= 1;
-    // }
-
-    this.counter += value;
-
-  }
-
-  public reset():void{
-
-    this.counter = 10;
-
-  }
 
 }
 
